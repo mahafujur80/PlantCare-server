b6ec27d7bc8d2f7eedec8546dfd00f0c13eccc70
MAHAFUJUR RAHMAN MUNNA <mahafujurrahman4480@gmail.com>
2026-07-15 16:18:15 +0600

    again fix prob

--- index.ts
+++ index.ts
@@ -46,12 +46,8 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-  const plantDB = client.db("PlantCare");
-  const plantCollection = plantDB.collection("Plants");
+const plantDB = client.db("PlantCare");
+const plantCollection = plantDB.collection("Plants");
 
 
   // user operations
@@ -166,15 +162,13 @@ app.delete('/api/delete/plant', verifyToken, async (req: Request, res: Response)
 });
 
 
-       // Send a ping to confirm a successful connection
+client
+  .connect()
+  .then(async () => {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+  })
+  .catch(console.error);
 
 
 
